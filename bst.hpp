df0ace3ec46c9e8e308599813e60b11f29d2e927
Phyll <thetechnologicalphyllielocal@gmail.com>
2025-03-23 21:03:48 +0300

    bst.hpp

--- bst.hpp
+++ bst.hpp
@@ -1,8 +1,9 @@
 // bst.hpp
 
-#ifndef BST_H
-#define BST_H
+#ifndef BST_HPP
+#define BST_HPP
 
+#include <sstream>
 #include <ostream>
 #include <type_traits>
 
@@ -31,5 +32,51 @@ private:
   static void inorder(Node*, std::ostream&);
   Node *insert(Node*, T);
 };
+
+template <class T, typename U>
+BST<T, U>::BST(void) : root{ nullptr } {}
+
+template <class T, typename U>
+BST<T, U> &BST<T, U>::insert(T val)
+{
+  root = insert(root, val);
+  return *this;
+}
+
+template <class T, typename U> void
+BST<T, U>::inorder(Node *node, std::ostream &os)
+{
+  if (!node) return;
+  inorder(node->left, os);
+  os << node->data << ' ';
+  inorder(node->right, os);
+}
+
+template <class T, typename U>
+typename BST<T, U>::Node *
+BST<T, U>::insert(Node *node, T val)
+{
+  if (!node)
+    return new Node{ val };
+  
+  if (val < node->data)
+    node->left = insert(node->left, val);
+  else
+    node->right = insert(node->right, val);
+  
+  return node;
+}
+
+
+template <class T, typename U> std::ostream &
+operator<<(std::ostream &out, const BST<T, U> &tree)
+{
+  std::ostringstream ss;
+  BST<T>::inorder(tree.root, ss);
+  std::string contents = ss.str();
+  if (!contents.empty())
+    contents.pop_back();
+  return out << "[ " << contents << " ]";
+}
 
 #endif
